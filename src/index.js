194e028fad0b2095442619ebcc7f0c443fcc92b0
Феникс Женя <zheka737@gmail.com>
2019-10-17 17:58:16 +0300

    dwd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import _ from "lodash";
 
 function component() {
     const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    const btn = document.createElement('button');
+
+
+    btn.innerHTML = 'Click me and check the console';
+    btn.onclick = onButtonClick;
+
+    element.appendChild(btn);
+
     return element;
   }
   
   document.body.appendChild(component());
+
+function getComponent(){
+    return import(/* webpackChunkName: "lodash" */ 'lodash').then(({default: _}) => {
+        const element = document.createElement('div');
+
+        element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+        return element;
+    }).catch(error => 'An error occured while loading component')
+}
+
+
+
+function onButtonClick(){
+    getComponent().then(component => {
+        document.body.appendChild(component);
+    })
+}
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        chunkFilename: '[name].bundle.js'
+    }
+};
